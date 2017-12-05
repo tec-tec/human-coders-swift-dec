1b167690a7143034be1a2e84d620be901fadf7ab
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-12-05 17:21:46 +0100

    Model first draft

---
+++ Playgrounds/Model.playground/Contents.swift
@@ -0,0 +1,60 @@
+//: Playground - noun: a place where people can play
+
+import Foundation
+
+struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+}
+
+enum PokemonType {
+    case fire
+    case water
+    case electric
+    case unknown
+}
+
+struct Pokemon {
+    var name: String
+    var level: Int
+    var force: Float {
+        return Float(level) * 1.5
+    }
+    var isCaptured: Bool
+    var captureLocation: Coordinate?
+    var captureDate: Date?
+    var weight: Float
+    let type: PokemonType
+
+    mutating func captured() {
+        guard !isCaptured else { return }
+        isCaptured = true
+        captureDate = Date()
+        captureLocation = Coordinate(latitude: 0.0, longitude: 0.0)
+    }
+
+    func levelUp() {
+        //To Do
+    }
+}
+
+class Pokedex {
+
+    var pokemons = [Pokemon]()
+
+    func add(_ p: Pokemon) {
+        pokemons.append(p)
+    }
+
+    func list() -> [Pokemon] {
+        return pokemons
+    }
+
+    func remove(_ p: Pokemon) {
+
+    }
+
+    func listOrderedByName() -> [Pokemon] {
+        
+    }
+}
