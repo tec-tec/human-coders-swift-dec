190ba01acc1cba3d402884fb18e6cbdc9a2347f7
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-12-06 15:36:33 +0100

    Ajout projet dmo

---
+++ Projets/PokeList/PokeList/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  PokeList
+//
+//  Created by Ludovic Ollagnier on 06/12/2017.
+//  Copyright © 2017 Human Coders. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
