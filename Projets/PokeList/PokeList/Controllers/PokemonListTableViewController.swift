1b2a1d0dc7d30ca9f6d53cf75dff0f66f96105e0
Ludovic Ollagnier <ludovic@ollagnier.me>
2017-12-08 12:45:01 +0100

    Implementation datasource

--- Projets/PokeList/PokeList/Controllers/PokemonListTableViewController.swift
+++ Projets/PokeList/PokeList/Controllers/PokemonListTableViewController.swift
@@ -10,14 +10,13 @@ import UIKit
 
 class PokemonListTableViewController: UITableViewController {
 
+    let dex = Pokedex()
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        dex.add(Pokemon(name: "Pikachu", level: 1, isCaptured: false, captureLocation: nil, captureDate: nil, weight: 3, type: .electric))
+        dex.add(Pokemon(name: "Carapuce", level: 12, isCaptured: false, captureLocation: nil, captureDate: nil, weight: 3, type: .water))
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,35 +26,31 @@ class PokemonListTableViewController: UITableViewController {
 
     // MARK: - Table view data source
 
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return dex.list().count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pokemonCell", for: indexPath)
 
         // Configure the cell...
+        let currentPokemon = dex.list()[indexPath.row]
+
+        cell.textLabel?.text = currentPokemon.name
+        cell.detailTextLabel?.text = "Niveau : \(currentPokemon.level)"
 
         return cell
     }
-    */
 
-    /*
+
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
         // Return false if you do not want the specified item to be editable.
         return true
     }
-    */
 
-    /*
+
+
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
@@ -65,22 +60,6 @@ class PokemonListTableViewController: UITableViewController {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
 
     /*
     // MARK: - Navigation
